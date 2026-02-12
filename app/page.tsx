3406c86fcbee88aec3dc3ce1a0591eb01c9e76a8
Rafia Zafar <rafiazafar75@gmail.com>
2026-02-12 23:14:41 +0500

    Made the folder structure
    Made the folder structure, so that it isn't a problem as I proceed ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-'use client'
+"use client";
+import TextAnalyser from "@/src/components/TextAnalyser";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="p-4 border-amber-600 border-4">Cognitive Insight Web</div>
-  );
+	return (
+		<>
+			<div className="p-4 border-amber-600 border-4">
+				Cognitive Insight Web
+			</div>
+
+			<TextAnalyser />
+		</>
+	);
 }
---
+++ src/components/TextAnalyser.tsx
@@ -0,0 +1,7 @@
+
+
+export default function TextAnalyser() {
+    return(
+        <div className="p-4 border-amber-600 border-4">Text Analyser</div>
+    )
+}
