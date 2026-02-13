98592cc2f364a719ff5117176d70046ba1327a8a
Rafia Zafar <rafiazafar75@gmail.com>
2026-02-14 01:16:26 +0500

    Added Mock Analyser
    Added Mock Analyser

--- src/components/TextAnalyser.tsx
+++ src/components/TextAnalyser.tsx
@@ -1,34 +1,57 @@
 "use client";
+import React, { useState } from "react";
 
-function TextBox({
-	label,
-	readOnly = false,
-}: {
+type TextBoxProps = {
 	label: string;
 	readOnly?: boolean;
-}) {
+	value?: string;
+	onChange?: (value: string) => void;
+};
+function TextBox({ label, readOnly, value, onChange }: TextBoxProps) {
 	return (
 		<div className="flex flex-col w-full md:w-1/2">
 			<label className="mb-2 text-white font-semibold"> {label}</label>
 			<textarea
 				className="w-full h-40 p-3 border border-gray-300 text-white rounded-md resize-none"
 				readOnly={readOnly}
+				value={value}
+				onChange={(e) => onChange && onChange(e.target.value)}
 			/>
 		</div>
 	);
 }
 
 export default function TextAnalyser() {
+	const [inputText, setInputText] = useState<string>("");
+    const [result, setResult] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleTextAnalyser = () =>{
+        if (inputText.trim() === "") {
+            alert("Please enter some text to analyze.");
+            return;
+        }
+
+       
+        setTimeout(() => {
+            setResult(`Text Length: ${inputText.length} characters`);
+             setLoading(false);
+        }, 1500)
+    }
 	return (
 		<>
-			<div>
+			<div className="container mx-auto p-3">
 				<div className="flex flex-col md:flex-row gap-6 w-full max-w-5xl ">
-					<TextBox label="Input text here" />
-					<TextBox label="Analysis results" readOnly={true} />
+					<TextBox
+						label="Input text here"
+						value={inputText}
+						onChange={setInputText}
+					/>
+					<TextBox label="Analysis results" readOnly={true} value={result} />
 				</div>
 				<div className="mt-6">
-					<button className="px-6 py-3 bg-amber-600 text-white rounded-md hover:bg-amber-700">
-						Analyze Text
+					<button onClick={handleTextAnalyser} className="px-6 py-3 bg-amber-600 text-white rounded-md hover:bg-amber-700">
+						{loading ? "Analyzing..." : "Analyze Text"}
 					</button>
 				</div>
 			</div>
